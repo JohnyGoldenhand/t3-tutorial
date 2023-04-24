4c0bde764ad187e5594828bbef0c16799a3276e6
Remigiusz Mikolajewicz <r.mikolajewicz@interia.pl>
2023-04-24 09:55:45 +0200

    create Router for posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { SignInButton, useUser, SignOutButton } from "@clerk/nextjs";
+import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -16,6 +19,11 @@ const Home: NextPage = () => {
         <div className="flex ">
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
         </div>
       </main>
     </>
